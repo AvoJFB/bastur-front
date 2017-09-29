ff57c34826db92edef5d3d68135027bb8ebbfdb1
Avetik Harutyunyan <avetik.harutyunyan1@gmail.com>
2017-09-29 16:13:48 +0300

    Update webpack config

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [
+          path.resolve(__dirname, 'src'),
+        ],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'es2015', 'stage-0'],
+        },
+      },
+      {
+        test: /\.jsx?$/,
+        enforce: 'pre',
+        include: [
+          path.resolve(__dirname, 'src'),
+        ],
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+    ],
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, 'src'),
+    ],
+    extensions: ['.js', '.jsx', '.json', '.css'],
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    hot: true,
+  },
+};
